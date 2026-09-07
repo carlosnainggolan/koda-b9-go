310e45de2186f1d59b85409646cb73157958e150
carlos <carlosnainggolan@gmail.com>
2026-09-07 13:40:36 +0700

    refactor: update main to print area and keliling separately

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(gabungan(8,5))
+	a, k := gabungan(8,5)
+	fmt.Printf("Luas: %d\n", a)
+	fmt.Printf("Keliling: %d\n", k)
 }
 
 // func greet (name string) {
