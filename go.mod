6b4b8ba062dc0a6b8bb2822e05c3abae86bd8ba5
carlos <carlosnainggolan@gmail.com>
2026-09-07 10:28:00 +0700

    feat: first init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/carlosnainggolan/koda-b9-git
+
+go 1.27.1
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	const name string = "carlos"
+	fmt.Println("Hello" + name)
+	fmt.Println("World")
+}
